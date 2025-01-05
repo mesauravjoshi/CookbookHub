a02fefc1b605b6e85738a98eb4ca5167e0845c82
saurav joshi <mesauravjoshii@gmail.com>
2025-01-05 20:46:18 +0530

    update

--- node/routes/admin/admin_bookmark.js
+++ node/routes/admin/admin_bookmark.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../../models/User'); 
 const Recipe = require('../../models/Recipe'); 
 const Bookmark = require('../../models/Bookmarks'); 
 const {jwtAuthMiddleware } = require('../../jwt');
